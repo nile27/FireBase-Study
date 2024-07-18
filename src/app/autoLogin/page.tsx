c6583c8c6c2d30d3846a72386ad52a9f28ec53a6
nile27 <ddj03104@gmail.com>
2024-07-18 22:59:08 +0900

    feat: 자동로그인

---
+++ src/app/autoLogin/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState } from "react";
+import {
+  signInWithEmailAndPassword,
+  setPersistence,
+  browserLocalPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig"; // firebase 설정 파일에서 가져옵니다.
+
+const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [rememberMe, setRememberMe] = useState<boolean>(false);
+
+  const handleSignIn = async () => {
+    try {
+      const persistence = rememberMe
+        ? browserLocalPersistence
+        : browserSessionPersistence;
+      await setPersistence(auth, persistence);
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("로그인 성공");
+    } catch (error) {
+      console.error("로그인 실패:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <div>
+        <input
+          type="checkbox"
+          checked={rememberMe}
+          onChange={() => setRememberMe(!rememberMe)}
+        />
+        <label>자동 로그인</label>
+      </div>
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/app/loginAuto/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig"; // firebase 설정 파일에서 가져옵니다.
+
+const Dashboard: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return (
+      <div>
+        <h1>Not signed in</h1>
+        <a href="/signin">Sign In</a>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Welcome, {user.email}</h1>
+      <button
+        onClick={() => {
+          signOut(auth);
+          setUser(null);
+        }}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+};
+
+export default Dashboard;
